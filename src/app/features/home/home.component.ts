f239359709bb7d62656e212263f697fbdf19409b
Luiz <luizinhon@hotmail.com>
2025-03-19 21:29:02 -0300

    feat: load products in the home component

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
-import { ProductCardComponent } from './components/product-card/product-card.component';
 import { RouterLink } from '@angular/router';
+import { CurrencyPipe } from '@angular/common';
+import { ProductCardComponent } from './components/product-card/product-card.component';
+import { LoadingComponent } from '../../shared/components/loading/loading.component';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../shared/models/product.model';
 
 @Component({
   selector: 'app-home',
-  imports: [ProductCardComponent, RouterLink],
+  imports: [ProductCardComponent, RouterLink, CurrencyPipe, LoadingComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  products: Product[] = [];
+  loading = false;
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.loading = true;
+    this.productService.getProducts().subscribe({
+      next: (products) => (this.products = products),
+      error: () => (this.loading = false),
+      complete: () => (this.loading = false),
+    });
+  }
+}
