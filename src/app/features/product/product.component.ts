928c952588668c42507a73cd14384dcb5a0004ca
Luiz <luizinhon@hotmail.com>
2025-03-29 00:05:35 -0300

    feat: add product to cart

--- src/app/features/product/product.component.ts
+++ src/app/features/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CurrencyPipe } from '@angular/common';
 import { LoadingComponent } from '../../shared/components/loading/loading.component';
@@ -6,6 +6,7 @@ import { ShowMoreComponent } from './components/show-more/show-more.component';
 import { ProductService } from '../../services/product/product.service';
 import { PaymentService } from '../../services/payment/payment.service';
 import { IProduct } from '../../shared/models/product.model';
+import { CartService } from '../../services/cart/cart.service';
 
 @Component({
   selector: 'app-product',
@@ -16,12 +17,19 @@ import { IProduct } from '../../shared/models/product.model';
 export class ProductComponent {
   product: IProduct | null = null;
   isLoading = false;
-  isAddCartBtnDisabled = false;
   isBuyNowBtnDisabled = false;
+  isProductInCart = computed(() => {
+    return this.product
+      ? this.cartService
+          .cart()
+          .some((cartProduct) => cartProduct.id === this.product!.id)
+      : false;
+  });
 
   constructor(
     private productService: ProductService,
     private paymentService: PaymentService,
+    private cartService: CartService,
     private readonly route: ActivatedRoute
   ) {}
 
@@ -39,6 +47,10 @@ export class ProductComponent {
         complete: () => (this.isLoading = false),
       });
     }
+  }
+
+  addToCart(product: IProduct) {
+    this.cartService.addProduct(product);
   }
 
   buyNow(product: IProduct) {
