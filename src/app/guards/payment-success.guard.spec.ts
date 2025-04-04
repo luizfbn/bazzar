c49bb8e5c96c8c3e62ee7bca491e35234b6544da
Luiz <luizinhon@hotmail.com>
2025-04-04 13:42:23 -0300

    test: guards

--- src/app/guards/payment-success.guard.spec.ts
+++ src/app/guards/payment-success.guard.spec.ts
@@ -1,17 +1,49 @@
 import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
-
+import { ActivatedRouteSnapshot, CanActivateFn, Router } from '@angular/router';
 import { paymentSuccessGuard } from './payment-success.guard';
 
 describe('paymentSuccessGuard', () => {
-  const executeGuard: CanActivateFn = (...guardParameters) => 
-      TestBed.runInInjectionContext(() => paymentSuccessGuard(...guardParameters));
+  let navigateMock: jest.Mock;
+
+  const executeGuard: CanActivateFn = (...guardParameters) =>
+    TestBed.runInInjectionContext(() =>
+      paymentSuccessGuard(...guardParameters)
+    );
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    navigateMock = jest.fn();
+    TestBed.configureTestingModule({
+      providers: [{ provide: Router, useValue: { navigate: navigateMock } }],
+    });
   });
 
   it('should be created', () => {
     expect(executeGuard).toBeTruthy();
+  });
+
+  it('should activate when session_id is provided', () => {
+    const fakeRoute: Partial<ActivatedRouteSnapshot> = {
+      queryParamMap: {
+        get: (name: string) => (name === 'session_id' ? '123' : null),
+      } as any,
+    };
+    const fakeState = { url: '/success' } as any;
+
+    const result = executeGuard(fakeRoute as any, fakeState);
+    expect(result).toBe(true);
+    expect(navigateMock).not.toHaveBeenCalled();
+  });
+
+  it('should not activate when session_id is not provided', () => {
+    const fakeRoute: Partial<ActivatedRouteSnapshot> = {
+      queryParamMap: {
+        get: jest.fn().mockReturnValue(null),
+      } as any,
+    };
+    const fakeState = { url: '/success' } as any;
+
+    const result = executeGuard(fakeRoute as any, fakeState);
+    expect(result).toBe(false);
+    expect(navigateMock).toHaveBeenCalledWith(['/']);
   });
 });
