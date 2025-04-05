3ff16cde18022066266251c200d0a0eaf5d26970
Luiz <luizinhon@hotmail.com>
2025-04-04 23:41:27 -0300

    test: fix tests for app and header component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,38 @@
 import { TestBed } from '@angular/core/testing';
+import {
+  HttpTestingController,
+  provideHttpClientTesting,
+} from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let httpMock: HttpTestingController;
+  let routeMock: Partial<ActivatedRoute>;
+
   beforeEach(async () => {
+    routeMock = {
+      snapshot: {
+        queryParamMap: {
+          get: jest.fn().mockReturnValue('test-id'),
+        },
+      },
+    } as unknown as ActivatedRoute;
+
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        { provide: ActivatedRoute, useValue: routeMock },
+      ],
     }).compileComponents();
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should create the app', () => {
--- src/app/layout/header/header.component.spec.ts
+++ src/app/layout/header/header.component.spec.ts
@@ -1,16 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ActivatedRoute } from '@angular/router';
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let routeMock: Partial<ActivatedRoute>;
 
   beforeEach(async () => {
+    routeMock = {
+      snapshot: {
+        queryParamMap: {
+          get: jest.fn().mockReturnValue('test-id'),
+        },
+      },
+    } as unknown as ActivatedRoute;
+
     await TestBed.configureTestingModule({
-      imports: [HeaderComponent]
-    })
-    .compileComponents();
+      imports: [HeaderComponent],
+      providers: [{ provide: ActivatedRoute, useValue: routeMock }],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
