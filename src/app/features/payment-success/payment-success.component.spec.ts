a26ca5337da1ccec503554870f0f8b28ef0a4016
Luiz <luizinhon@hotmail.com>
2025-04-05 01:10:36 -0300

    test: payment-success component

--- src/app/features/payment-success/payment-success.component.spec.ts
+++ src/app/features/payment-success/payment-success.component.spec.ts
@@ -1,16 +1,49 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ActivatedRoute, Router } from '@angular/router';
+import { of, throwError } from 'rxjs';
 import { PaymentSuccessComponent } from './payment-success.component';
+import { PaymentService } from '../../services/payment/payment.service';
+import { CartService } from '../../services/cart/cart.service';
 
 describe('PaymentSuccessComponent', () => {
   let component: PaymentSuccessComponent;
   let fixture: ComponentFixture<PaymentSuccessComponent>;
+  let paymentServiceMock: Partial<PaymentService>;
+  let cartServiceMock: Partial<CartService>;
+  let routeMock: Partial<ActivatedRoute>;
+  let routerMock: Partial<Router>;
+  const customerMock = { name: 'John Doe', email: 'johndoe@email.com' };
 
   beforeEach(async () => {
+    paymentServiceMock = {
+      getPaymentStatus: jest.fn().mockReturnValue(of(customerMock)),
+    };
+
+    cartServiceMock = {
+      setCart: jest.fn(),
+    };
+
+    routeMock = {
+      snapshot: {
+        queryParamMap: {
+          get: jest.fn().mockReturnValue('test-session-id'),
+        },
+      },
+    } as unknown as ActivatedRoute;
+
+    routerMock = {
+      navigate: jest.fn(),
+    };
+
     await TestBed.configureTestingModule({
-      imports: [PaymentSuccessComponent]
-    })
-    .compileComponents();
+      imports: [PaymentSuccessComponent],
+      providers: [
+        { provide: PaymentService, useValue: paymentServiceMock },
+        { provide: CartService, useValue: cartServiceMock },
+        { provide: ActivatedRoute, useValue: routeMock },
+        { provide: Router, useValue: routerMock },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(PaymentSuccessComponent);
     component = fixture.componentInstance;
@@ -19,5 +52,30 @@ describe('PaymentSuccessComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize and call getPaymentStatus with session_id', () => {
+    const spyGetPaymentStatus = jest.spyOn(component, 'getPaymentStatus');
+    component.ngOnInit();
+    expect(spyGetPaymentStatus).toHaveBeenCalledWith('test-session-id');
+  });
+
+  it('should set customer when paymentService.getPaymentStatus succeeds', () => {
+    component.getPaymentStatus('test-session-id');
+
+    expect(component.isLoading).toBe(false);
+    expect(component.customer).toEqual(customerMock);
+    expect(cartServiceMock.setCart).toHaveBeenCalledWith([]);
+  });
+
+  it('should navigate to / when paymentService.getPaymentStatus fails', () => {
+    paymentServiceMock.getPaymentStatus = jest
+      .fn()
+      .mockReturnValue(throwError(() => new Error()));
+
+    component.getPaymentStatus('test-session-id');
+
+    expect(component.isLoading).toBe(false);
+    expect(routerMock.navigate).toHaveBeenCalledWith(['/']);
   });
 });
